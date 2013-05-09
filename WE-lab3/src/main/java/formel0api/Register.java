525659afcd115ee2fb36798f4203ee8ee0926320
Lukas Kraenkl <studium@kraenkl.com>
2013-05-09 02:29:44 +0200

    Create user managing register

---
+++ WE-lab3/src/main/java/formel0api/Register.java
@@ -0,0 +1,67 @@
+/**
+ * In this bean all registered users are listed and managed
+ * @author: Lukas Kraenkl, David Pfahler and Johannes Deml
+ */
+package formel0api;
+
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+// Application Scoped means that the Klass will be saved during the whole Server session 
+// (solange Glassfish halt noch werkelt)
+import javax.faces.bean.ApplicationScoped;
+
+@ManagedBean
+@ApplicationScoped
+public class Register {
+
+    private static Register instance = null; 
+    private ArrayList<User> list = new ArrayList<User>();
+    
+    /**
+     * Creates a new Register (private -> Singleton Pattern)
+     */
+    private Register() {
+    }
+    
+    /**
+     * Returns or creates a instance of Register
+     * @return instance of Register
+     */
+    public static Register getInstance(){
+        if(instance==null){
+            instance = new Register();
+        }
+        return instance;
+    }
+    
+    /**
+     * Adds a new user in the list
+     * @param user 
+     */
+    public void addUser(User user){
+        list.add(user);
+    }
+    
+    /**
+     * Returns all Users
+     * @return user
+     */
+    public ArrayList<User> getAllUsers(){
+        return this.list;
+    }
+    
+    /**
+     * Returns the user of the given username
+     * @param username 
+     * @return user if there is a user with that username, otherwise NULL
+     */
+    public User getUser(String username){
+        for(User u : list){
+            if(u.getUsername().equals(username)){
+                return u;
+            }
+        }
+        return null;
+    }
+    
+}
