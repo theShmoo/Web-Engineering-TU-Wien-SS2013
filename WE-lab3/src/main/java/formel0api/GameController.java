6bed47912c2467c7cf043758a18a4646efe885f4
Johannes Deml <johannski0@gmail.com>
2013-05-10 15:59:52 +0200

    Change paths to resources, change variable names to first char lowercased

--- WE-lab3/src/main/java/formel0api/GameController.java
+++ WE-lab3/src/main/java/formel0api/GameController.java
@@ -8,15 +8,24 @@ package formel0api;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
-@ManagedBean
+@ManagedBean(name="gameController")
 @RequestScoped
 public class GameController {
 
+    Player player;
+    Player computer;
+    Game game;
+    
     /**
      * Creates a new instance of GameController
      */
     public GameController() {
+        player = new Player("TestPlayer");
+        computer = new Player("ComputerTestPlayer");
+        game = new Game(player,computer);
     }
+    
+    
     
     // TODO würfeln, beinhaltet beans wie game, player, dice
 
