473d412ad4d98784dfbf83619df3f24a7de2e8e5
Lukas Kraenkl <studium@kraenkl.com>
2013-05-30 20:11:12 +0200

    Twitter is now working
     - add TwitterImpl
     - add Twitter Unit Test

--- WE-lab4/src/main/java/tuwien/big/formel0/twitter/TwitterClientImpl.java
+++ WE-lab4/src/main/java/tuwien/big/formel0/twitter/TwitterClientImpl.java
@@ -1,6 +1,7 @@
 package tuwien.big.formel0.twitter;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
@@ -13,20 +14,30 @@ public class TwitterClientImpl implements ITwitterClient{
 
     @Override
     public void publishUuid(TwitterStatusMessage message) throws Exception {
-       
+ 
         Twitter tweet = new TwitterFactory().getInstance();
-        
-        RequestToken requestToken = tweet.getOAuthRequestToken();
-        AccessToken accessToken = null;
-        
-        while (accessToken == null) {
-            
-            //TODO
-            
+        try{
+            RequestToken requestToken = tweet.getOAuthRequestToken();
+            AccessToken accessToken = null;
+
+            while (accessToken == null) {
+                try{
+                    accessToken = tweet.getOAuthAccessToken(requestToken);
+                }
+                catch (TwitterException te){
+                   if(te.getStatusCode() == 401){
+                       // logger ausgabe
+                   } 
+                   throw te;
+                }
+            }    
         }
+        catch(IllegalStateException ie){
+            // Der Token ist bereits vorhanden oder einer der Keys ist nicht gesetzt
+            // DO NOTHING
+        }
+        
+        tweet.updateStatus(message.getTwitterPublicationString());
     
-    }
-    
-    
-    
+    }   
 }
