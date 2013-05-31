34ebd172f729cbdab6b3d9bde00bf0c8b1f9af85
Lukas Kraenkl <studium@kraenkl.com>
2013-05-31 18:53:22 +0200

    Test class rejected -> Erstellen eines Games zu umstaendlich

--- WE-lab4/src/test/java/test/entities/HighScoreServiceImplTest.java
+++ WE-lab4/src/test/java/test/entities/HighScoreServiceImplTest.java
@@ -1,11 +1,14 @@
 package test.entities;
 
+import formel0api.Game;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import tuwien.big.formel0.entities.Player;
+import tuwien.big.formel0.entities.Sex;
 import tuwien.big.formel0.soap.HighScoreServiceImpl;
 
 /**
@@ -30,8 +33,24 @@ public class HighScoreServiceImplTest {
     }
     
     @Test(expected=IllegalArgumentException.class)
-    public void giveNullShouldReturnException1(){
-        //foo
+    public void giveNullShouldReturnException(){
+        hssi.publishHighScore(null, null);
+    }
+    
+    // TODO 
+    // Testfall mit echtem Game sehr umstaendlich, deshalb hier nicht implementiert
+    
+    private Player getValidPlayer(){
+        Player player = new Player();
+        player.setName("lukas");
+        player.setBirthday("25.03.1992");
+        player.setSex(Sex.MALE);
+        return player;
+    }
+    
+    private Game getValidGame(){
+        Game game = new Game(null,null);        
+        return game;
     }
     
     
