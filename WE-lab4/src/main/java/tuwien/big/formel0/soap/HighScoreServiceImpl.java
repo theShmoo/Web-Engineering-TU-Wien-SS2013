ea09d0dd9fdc1d002ad118d4d92920abc3baffd3
Lukas Kraenkl <studium@kraenkl.com>
2013-05-31 18:38:50 +0200

    Implementing IHighScoreService and edit Interface JavaDoc

--- WE-lab4/src/main/java/tuwien/big/formel0/soap/HighScoreServiceImpl.java
+++ WE-lab4/src/main/java/tuwien/big/formel0/soap/HighScoreServiceImpl.java
@@ -1,27 +1,119 @@
 package tuwien.big.formel0.soap;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import formel0api.Game;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.ws.WebServiceRef;
+import tuwien.big.formel0.soap.*;
 import tuwien.big.formel0.entities.Player;
 
 /**
- *
- * @author Operator
+ * This class publishes the highscore in the Internet
+ * @author Lukas Kraenkl, David Pfahler, Johannes Deml
  */
 public class HighScoreServiceImpl implements IHighScoreService {
 
+    @WebServiceRef(wsdlLocation = "http://playground.big.tuwien.ac.at:8080/highscore/PublishHighScoreService?wsdl")
+    private static PublishHighScoreService service;
+
+    /**
+     * Creates a new HighScoreServiceImpl
+     */
+    public HighScoreServiceImpl() {
+        service = new PublishHighScoreService();
+    }
+    
     @Override
     public String publishHighScore(Game game, Player player) {
         
+        if(game==null || player==null){
+            throw new IllegalArgumentException("Die Werte zum publishen waren null");
+        }
         
-
-        //Request: generate RequestType
-        tuwien.big.formel0.soap.ObjectFactory requestFactory = new tuwien.big.formel0.soap.ObjectFactory();
-        HighScoreRequestType highScoreRequest = requestFactory.createHighScoreRequestType();
+        HighScoreRequestType highScoreRequest = new ObjectFactory().createHighScoreRequestType();
         highScoreRequest.setUserKey("34EphAp2C4ebaswu");
 
-        //TODO 
+        ObjectFactory universalFactory = new ObjectFactory();
         
-        return "";
+        TournamentType tt = universalFactory.createTournamentType();
+        
+        // Fuer start-date, end-date und registration-date nehmen immer das aktuelle Datum
+        tt.setStartDate(new XMLGregorianCalendarImpl(new GregorianCalendar()));
+        tt.setEndDate(new XMLGregorianCalendarImpl(new GregorianCalendar()));
+        tt.setRegistrationDeadline(new XMLGregorianCalendarImpl(new GregorianCalendar()));
+        
+        // Tournament -> Players
+        TournamentType.Players tplayers = universalFactory.createTournamentTypePlayers();
+        
+        // Tournament -> Players -> Player
+        TournamentType.Players.Player actualPlayer = new TournamentType.Players.Player();        
+        
+            // Username, Geschlecht und Geburtsdatum werden übergeben
+            actualPlayer.setGender(player.getSex().getLabel());
+            actualPlayer.setUsername(player.getName());
+        
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+            GregorianCalendar date = null;
+            try {
+                date = (GregorianCalendar) GregorianCalendar.getInstance();
+                date.setTime(sdf.parse(player.getBirthday()));
+            } catch (ParseException ex) {
+                // do nothing
+            }
+            actualPlayer.setDateOfBirth(new XMLGregorianCalendarImpl(date));
+            
+        tplayers.getPlayer().add(actualPlayer);
+        tt.setPlayers(tplayers);
+
+        // Runden
+        // Es wird genau eine Runde uebergeben, welche die number '0' haben muss.
+        TournamentType.Rounds rounds = universalFactory.createTournamentTypeRounds();
+        TournamentType.Rounds.Round round = universalFactory.createTournamentTypeRoundsRound();
+        round.setNumber(0);
+
+        // In der Runde ist genau ein Game enthalten, wobei Date wieder das aktuelle Datum ist
+        GameType game1 = universalFactory.createGameType();
+        game1.setDate(new XMLGregorianCalendarImpl(new GregorianCalendar()));
+        game1.setStatus("finished");
+        
+        // Duration ist die Anzahl in Sekunden, die während des Spiels verstrichen sind
+        Long tmp = game.getSpentTime() / 1000;
+        game1.setDuration(BigInteger.valueOf(tmp));
+        
+        // Unter winner uebergeben Sie den Gewinner des Spiels (entweder 'Computer' oder Name des Spielers
+        if (game.getLeader().getName().equals(player.getName())) {
+            game1.setWinner(player.getName());
+        } else {
+            game1.setWinner("Computer");
+        }
+
+        GameType.Players.Player gamePlayer = universalFactory.createGameTypePlayersPlayer();
+        GameType.Players gamePlayers = universalFactory.createGameTypePlayers();
+        gamePlayer.setRef(player.getName());
+        gamePlayers.getPlayer().add(gamePlayer);
+        game1.setPlayers(gamePlayers);
+
+        // Verknuepfen der Elemente
+        round.getGame().add(game1);
+        rounds.getRound().add(round);
+        tt.setRounds(rounds);
+        tt.setDescription(universalFactory.createDescriptionType());
+        
+        highScoreRequest.setTournament(tt);
+
+        String uuid = null;
+        try {
+            uuid = service.getPublishHighScorePort().publishHighScore(highScoreRequest);
+            
+        } catch (Exception ex) {
+            // do nothing
+            throw new Error("Beim publishen ist etwas daneben gegangen");
+        }
+        return uuid;
     
     }
     
--- WE-lab4/src/main/java/tuwien/big/formel0/soap/IHighScoreService.java
+++ WE-lab4/src/main/java/tuwien/big/formel0/soap/IHighScoreService.java
@@ -13,7 +13,8 @@ public interface IHighScoreService {
      * Publishes for a specific Player and Game the highscore 
      * @param game, not null
      * @param player, not null
-     * @return UUID (wichtig fuer Twitter - siehe Angabe Seite 3)
+     * @return UUID (wichtig fuer Twitter - siehe Angabe Seite 3), null wenn Fehler aufgetreten
+     * @throws IllegalArgumentException when game or player == null
      */
      public String publishHighScore(Game game,Player player);    
     
