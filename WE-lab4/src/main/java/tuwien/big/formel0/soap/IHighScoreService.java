b96a7ba52e032e7e95a5a3375fe81caf469a3ef5
Lukas Kraenkl <studium@kraenkl.com>
2013-05-31 17:09:15 +0200

    Specified Interface - IHighScoreService

--- WE-lab4/src/main/java/tuwien/big/formel0/soap/IHighScoreService.java
+++ WE-lab4/src/main/java/tuwien/big/formel0/soap/IHighScoreService.java
@@ -3,9 +3,18 @@ package tuwien.big.formel0.soap;
 import formel0api.Game;
 import tuwien.big.formel0.entities.Player;
 
-
+/**
+ * High Score Service Interface
+ * @author Lukas Kraenkl, David Pfahler, Johannes Deml
+ */
 public interface IHighScoreService {
     
+    /**
+     * Publishes for a specific Player and Game the highscore 
+     * @param game, not null
+     * @param player, not null
+     * @return UUID (wichtig fuer Twitter - siehe Angabe Seite 3)
+     */
      public String publishHighScore(Game game,Player player);    
     
 }
